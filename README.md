04e3899ca871a48ad548d2c39d2f1156eeacb4d4
angela1C <fange21@icloud.com>
2021-01-06 01:26:27 +0000

    got the web service working with values outputting to the index page as well as the routes, need to tidy up, then cut down the notebook

--- README.md
+++ README.md
@@ -7,9 +7,11 @@ This repository contains the Jupyter notebook x.ipynb which contains the main bo
 
 In this project you must create a web service that uses machine learning to make pre- dictions based on the data set powerproduction available on Moodle. The goal is to produce a model that accurately predicts wind turbine power output from wind speed values, as in the data set. You must then develop a web service that will respond with predicted power values based on speed values sent as HTTP requests. Your submission must be in the form of a git repository containing, at a minimum, the following items.
 Jupyter notebook that trains a model using the data set. In the notebook you should explain your model and give an analysis of its accuracy.
-Python script that runs a web service based on the model, as above.
-Dockerfile to build and run the web service in a container.
-Standard items in a git repository such as a README. To enhance your submission, you might consider developing and comparing more than one model. Rest assured, all the above concepts will be explored in lecture videos and other materials in the coming semester.
+- Python script that runs a web service based on the model, as above.
+- Dockerfile to build and run the web service in a container.
+- Standard items in a git repository such as a README. 
+- To enhance your submission, you might consider developing and comparing more than one model. 
+- Rest assured, all the above concepts will be explored in lecture videos and other materials in the coming semester.
 
 
 ### How to download the repository
--- application.py
+++ application.py
@@ -6,10 +6,16 @@ import numpy as np
 import joblib
 
 model_poly3 = joblib.load('model_poly3.joblib')
+mod_poly3 = joblib.load('model_poly3.pkl')
+
 
 import tensorflow.keras as kr
+from tensorflow.keras.models import load_model
 
 model = kr.models.load_model("model.h5")
+
+NNmodel1 = load_model('model.h5')
+NNmodel2 = load_model('model2.h5')
 
 # not using a separate file like in datarep project dao
 # just testing here for now 
@@ -26,7 +32,11 @@ def predict_power(speed):
     return(speed*100)
 
 def predict_power_poly3(speed):
-  
+  return "polynomial"
+
+
+
+
 
 
 # Create a new web app.
@@ -38,20 +48,64 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 def home():
   return app.send_static_file('index.html')
 
+# need to get this in the right dimensions
+windspeed = 12
+
 
   # Add model1 prediction route.
-@app.route('/api/model1/<int:speed>')
-def power1(speed):
-  #return predict_power(speed)
+@app.route('/api/neuralnetwork1/<int:speed>')
+
+def predict_power_NN_model1(speed):
+  #speed = np.array(speed)
+
+  if speed < 3:
+    return {"value": 0}
+  elif speed > 24.4:
+    return {"value": 0}
+  else:
+    prediction = NNmodel1.predict([speed])
+    return {"value": str(prediction[0][0])} 
+
+
+@app.route('/api/neuralnetwork2/<int:speed>')
+def predict_power_NN_model2(speed):
+  if speed < 3:
+    return {"value": 0}
+  elif speed > 24.4:
+    return {"value": 0}
+  else:
+    prediction = NNmodel1.predict([speed])
+    return {"value": str(prediction[0][0])} 
+
+
+    
+  #prediction = NNmodel1([speed])
   
-  return {"value": predict_power(speed)}
+  #return {"value": str(prediction[0][0])}
 
 # Add model2 prediction route.
-@app.route('/api/model2/<int:speed>')
+@app.route('/api/neuralnetwork2/<int:speed>')
 def power2(speed):
   #return predict_power(speed)
   
   return {"value": predict_power(speed)}
   
+# Add polynomial3 prediction route.
+# need to put upper and lower limits on this to stop negative values
+@app.route('/api/polynomial3/<int:speed>')
+def power3(speed):
+  #return predict_power(speed)
+  # I think I need to transform the wind speeds here.
+  # maybe just do so manually or by squaring and cubing the inputs as it is only one at a time
+  if speed >24.4:
+    return(0)
+  
+  if speed <3:
+    return (0)
+  #speed = np.array([[speed, speed**2, speed**3]])
+  else:
+    prediction = model_poly3.predict([[speed, speed**2, speed**3]])
+  #prediction = model_poly3.predict([[speed, speed**2, speed*3]])
 
+    return {"value": str(prediction[0][0])}
 
