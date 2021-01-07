3b5b5e8dd921f73e99fbc7c19152a77497c6751e
angela1C <fange21@icloud.com>
2021-01-07 18:07:50 +0000

    working on web app, output power output range to page

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ import tensorflow.keras as kr
 from tensorflow.keras.models import load_model
 
 model = kr.models.load_model("model.h5")
-
+# two neural network models
 NNmodel1 = load_model('model.h5')
 NNmodel2 = load_model('model2.h5')
 
@@ -34,11 +34,6 @@ def predict_power(speed):
 def predict_power_poly3(speed):
   return "polynomial"
 
-
-
-
-
-
 # Create a new web app.
 app = Flask(__name__, static_url_path='', static_folder='static')
 
@@ -48,9 +43,6 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 def home():
   return app.send_static_file('index.html')
 
-# need to get this in the right dimensions
-windspeed = 12
-
 
   # Add model1 prediction route.
 @app.route('/api/neuralnetwork1/<int:speed>')
@@ -58,54 +50,83 @@ windspeed = 12
 def predict_power_NN_model1(speed):
   #speed = np.array(speed)
 
-  if speed < 3:
-    return {"value": 0}
+  if speed < 3.5:
+    NN1pred = NNmodel1.predict([speed])
+    min = 0
+    NN1pred = NNmodel1.predict([speed])
+    return {"value": NN1pred}
   elif speed > 24.4:
     return {"value": 0}
   else:
-    prediction = NNmodel1.predict([speed])
-    return {"value": str(prediction[0][0])} 
+    NN1pred = NNmodel1.predict([speed])
+    return {"value": str(NN1pred[0][0])} 
 
 
 @app.route('/api/neuralnetwork2/<int:speed>')
 def predict_power_NN_model2(speed):
-  if speed < 3:
+  if speed < 3.5:
     return {"value": 0}
   elif speed > 24.4:
     return {"value": 0}
   else:
-    prediction = NNmodel1.predict([speed])
-    return {"value": str(prediction[0][0])} 
+    NN2pred = NNmodel2.predict([speed])
+    return {"value": str(NN2pred[0][0])} 
 
 
-    
-  #prediction = NNmodel1([speed])
-  
-  #return {"value": str(prediction[0][0])}
-
-# Add model2 prediction route.
-@app.route('/api/neuralnetwork2/<int:speed>')
-def power2(speed):
-  #return predict_power(speed)
-  
-  return {"value": predict_power(speed)}
   
 # Add polynomial3 prediction route.
 # need to put upper and lower limits on this to stop negative values
 @app.route('/api/polynomial3/<int:speed>')
-def power3(speed):
+def predict_power_poly3_model(speed):
   #return predict_power(speed)
   # I think I need to transform the wind speeds here.
   # maybe just do so manually or by squaring and cubing the inputs as it is only one at a time
-  if speed >24.4:
+  if speed >24.3:
+    #poly3pred = model_poly3.predict([[speed, speed**2, speed**3]])
+    return {"value": 0}
+  
+  if speed < 3.5:
+    poly3pred = model_poly3.predict([[speed, speed**2, speed**3]])[0][0]
+    NN1pred = NNmodel1.predict([speed])[0][0]
+
+
+    return {"value": min(0,poly3pred) }
+  #speed = np.array([[speed, speed**2, speed**3]])
+  else:
+    poly3pred = model_poly3.predict([[speed, speed**2, speed**3]])[0][0]
+  #prediction = model_poly3.predict([[speed, speed**2, speed*3]])
+
+    return {"value": str(poly3pred)}
+
+
+@app.route('/api/polynomial4/<int:speed>')
+def predict_power_poly4_model(speed):
+  if speed >24.3:
     return(0)
   
   if speed <3:
     return (0)
   #speed = np.array([[speed, speed**2, speed**3]])
   else:
-    prediction = model_poly3.predict([[speed, speed**2, speed**3]])
-  #prediction = model_poly3.predict([[speed, speed**2, speed*3]])
+    # need to save and import poly4 model first
+    poly4 = model_poly3.predict([[speed, speed**2, speed**3, speed**4]])
 
-    return {"value": str(prediction[0][0])}
+@app.route('/api/range/<int:speed>')
+def predict_power_range(speed):
+  # Turbine will always be powered off at speed values greater than 24.4
+  if speed >24.3:
+    return({"min":str(0), "max":str(0)})
 
+  if speed ==0:
+    return({"min":str(0), "max":str(0)})
+
+  elif speed >0 and speed <24.3:
+    # polynomial model is predicting negative values
+    poly3pred = max(0,model_poly3.predict([[speed, speed**2, speed**3]])[0][0])
+    #poly4 = model_poly4.predict([[speed, speed**2, speed**3, speed**4]])
+    NN1pred = NNmodel1.predict([speed])[0][0]
+    NN2pred = NNmodel2.predict([speed])[0][0]
+ 
+
+    return {"min":str(min(poly3pred,NN1pred, NN2pred)),"max":str(max(poly3pred,NN1pred, NN2pred ))}
+    
